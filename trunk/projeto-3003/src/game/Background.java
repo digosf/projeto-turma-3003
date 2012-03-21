f2ef9fa1fda042a7be5e3ac1c73bc557ed25f150
adrianomonteiro.nave <adrianomonteiro.nave@d8bfa6d5-b821-507e-dfd0-f08592b7b30e>
2012-03-21 17:49:26 +0000

    adicionando Background.java e Logo.java

---
+++ trunk/projeto-3003/src/game/Background.java
@@ -0,0 +1,29 @@
+    package game;
+    import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+
+public class Background extends GameObject{
+	
+		
+	public Background(float x, float y, int w, int h, String image, SoundClip sound) {
+		 super(x, y, w, h, image,sound);
+		 
+		 
+		
+	}
+
+	private static final long serialVersionUID = 1L;		
+
+		public void load(String string) 
+		{		
+				
+			
+		}
+
+		public void Draw(Graphics2D g2d) 
+		{		
+			
+			
+		}
+	}
+
