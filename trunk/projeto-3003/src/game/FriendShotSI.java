a33bd8e4e0330faa197ffb76d815444fe9b7a8a2
felipepozo.nave@gmail.com <felipepozo.nave@gmail.com@d8bfa6d5-b821-507e-dfd0-f08592b7b30e>
2012-03-21 17:35:57 +0000

    - Recomitando classe de tiro do player.

--- trunk/projeto-3003/src/game/FriendShotSI.java
+++ trunk/projeto-3003/src/game/FriendShotSI.java
@@ -3,12 +3,11 @@ package game;
 public class FriendShotSI extends Shot
 {
 	
-	
+	//Alterado por felipe, nao descomente este codigo.
 	public FriendShotSI(float x, float y, int w, int h, String image, SoundClip sound)
 	{
 		super(x, y, w, h, image, sound);
 		speedY = 1;
-			
 	}
 	public void Update()
 	{
