e4737f2c5738fe03c5c81c0e54a33e997159e1fa
alexanderbarbosa.nave@gmail.com <alexanderbarbosa.nave@gmail.com@d8bfa6d5-b821-507e-dfd0-f08592b7b30e>
2012-03-21 17:40:28 +0000

    - Commitando a classe EnemySI.java - Space Invaders

---
+++ trunk/projeto-3003/src/game/EnemySI.java
@@ -0,0 +1,21 @@
+package game;
+import java.awt.Graphics;
+
+public class EnemySI extends GameObject
+{
+	
+	
+public EnemySI(float x, float y, int w, int h, String image, SoundClip sound) {
+		super(x, y, w, h, image, sound);
+		
+		
+	}	
+
+public void MoveEnemy(float x, float y)
+{
+	
+	super.x += x;
+	super.y += y;
+}
+	
+}
